268b539871ec4829fecda4241f6058634b4faf4a
Olly <56711484+Wologman@users.noreply.github.com>
2022-06-04 18:24:46 +1200

    Add files via upload

---
+++ bird_positions.py
@@ -0,0 +1,28 @@
+# Find the positions of the hawaiian birds within the list
+
+
+all_birds = 'afrsil1 akekee akepa1 akiapo akikik amewig aniani apapan arcter \
+                      barpet bcnher belkin1 bkbplo bknsti bkwpet blkfra blknod bongul \
+                      brant brnboo brnnod brnowl brtcur bubsan buffle bulpet burpar buwtea \
+                      cacgoo1 calqua cangoo canvas caster1 categr chbsan chemun chukar cintea \
+                      comgal1 commyn compea comsan comwax coopet crehon dunlin elepai ercfra eurwig \
+                      fragul gadwal gamqua glwgul gnwtea golphe grbher3 grefri gresca gryfra gwfgoo \
+                      hawama hawcoo hawcre hawgoo hawhaw hawpet1 hoomer houfin houspa hudgod iiwi incter1 \
+                      jabwar japqua kalphe kauama laugul layalb lcspet leasan leater1 lessca lesyel lobdow lotjae \
+                      madpet magpet1 mallar3 masboo mauala maupar merlin mitpar moudov norcar norhar2 normoc norpin \
+                      norsho nutman oahama omao osprey pagplo palila parjae pecsan peflov perfal pibgre pomjae puaioh \
+                      reccar redava redjun redpha1 refboo rempar rettro ribgul rinduc rinphe rocpig rorpar rudtur ruff \
+                      saffin sander semplo sheowl shtsan skylar snogoo sooshe sooter1 sopsku1 sora spodov sposan \
+                      towsol wantat1 warwhe1 wesmea wessan wetshe whfibi whiter whttro wiltur yebcar yefcan zebdov'.split()
+
+hawaiian_birds = ["akiapo", "aniani", "apapan", "barpet", "crehon", "elepai", "ercfra", "hawama", "hawcre", "hawgoo", "hawhaw", 
+                 "hawpet1", "houfin", "iiwi", "jabwar", "maupar", "omao", "puaioh", "skylar", "warwhe1", "yefcan"]
+
+
+index_list=[]
+for bird in hawaiian_birds:
+    index_list.append(all_birds.index(bird))
+
+print(index_list)
+print(len(index_list))
+print(len(all_birds))
